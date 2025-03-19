60e628321f1f80419b6c66c3a5a18dd699363c37
moabdels <mkabdelsater@gmail.com>
2025-03-19 16:07:43 +0200

    Add memsfty.h which will contain the Arena implementation and some generic memory safety functionality

---
+++ inc/memsfty.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memsfty.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 15:24:35 by moabdels          #+#    #+#             */
+/*   Updated: 2025/03/19 15:50:38 by moabdels         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MEMSFTY_H
+# define MEMSFTY_H
+
+// ! How to Define Arrays
+// typedef struct {
+// 	int*	items;
+// 	int		length;
+// 	int		capacity;
+// }	IntArray
+
+// ! Then, define a Getter function, this avoids messing up array bounds.
+// int	IntArray_Get(IntArray array, int index)
+// {
+// 	if (index >= 0 && index < array.length)
+// 		return array.items[index];
+// 	return 0; // some default value that makes sense or something like
+//				raise(SIGTRAP)
+// }
+// ? you don't have to worry about performance. the compiler is smart enough
+// ? to optimize the if statement out
+
+// ! Storing references to other objects:
+// ? If you're referencing an object in an array don't include the
+// ? pointer itself as a field if possible, include the index in
+// ? that array, so for example:
+
+// typedef struct User
+//{
+//	String name;
+//	int friendIndex; // the index of User in the friends array of other user
+//}
+
+typedef struct
+{
+	char*	chars;
+	int		length;
+}	String;
+
+#endif
--- inc/minishell.h
+++ inc/minishell.h
@@ -6,9 +6,12 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:02:04 by moabdels          #+#    #+#             */
-/*   Updated: 2025/03/19 15:17:01 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:45:41 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 #include "../lib/libft/libft.h"
 
@@ -18,7 +21,6 @@ typedef struct env_list{
     char *data;
     struct env_list *next;
 }env_list;
-
 
 env_list *create_env_list(char **env);
 
@@ -32,3 +34,4 @@ void print_env(env_list *head);
 
 char *ft_expand(char *name, env_list *head);
 
+#endif
