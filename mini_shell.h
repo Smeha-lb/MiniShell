40893fbd894187c9bc8b97e773e97fd7d53b9158
Carl Samaha <carlsamaha1@gmail.com>
2025-03-21 13:01:55 +0200

    added string.c

--- mini_shell.h
+++ mini_shell.h
@@ -7,6 +7,10 @@ typedef struct env_list{
     struct env_list *next;
 }env_list;
 
+typedef struct string{;
+    char *value;
+    int length;
+}string;
 
 env_list *make_linked_list(char **env);
 
@@ -14,9 +18,10 @@ env_list *make_linked_list(char **env);
 int check_list_dup(char *data, env_list *head);
 
 
+void export(env_list *head);
 void add_env_var(char *data, env_list *head);
 void print_env(env_list *head);
 
 
-char *ft_expand(char *name, env_list *head);
+char *expand(char *name, env_list *head);
 
