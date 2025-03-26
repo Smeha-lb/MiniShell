a0e8a6ca5d745e24f41fe31b38475d78fc93923d
moabdels <mkabdelsater@gmail.com>
2025-03-26 12:31:39 +0200

    Clean up after deleting the Arena code

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:02:04 by moabdels          #+#    #+#             */
-/*   Updated: 2025/03/24 15:00:30 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/03/26 12:31:20 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,22 @@
 
 #include "../lib/libft/libft.h"
 #include "../lib/ft_printf/ft_printf.h"
+#include "../inc/ft_memsafety.h"
 
 typedef struct env_list{
-    char *name;
-    char *value;
-    char *data;
-    struct env_list *next;
-}env_list;
+	char *name;
+	char *value;
+	char *data;
+	struct env_list *next;
+}	env_list;
 
-env_list *create_env_list(char **env);
+env_list	*create_env_list(char **env);
 
+int	check_list_dup(char *data, env_list *head);
 
-int check_list_dup(char *data, env_list *head);
+void	add_env_var(char *data, env_list *head);
+void	print_env(env_list *head);
 
-
-void add_env_var(char *data, env_list *head);
-void print_env(env_list *head);
-
-
-char *ft_expand(char *name, env_list *head);
+char	*ft_expand(char *name, env_list *head);
 
 #endif
--- src/ft_memsafety_one.c
+++ src/ft_memsafety_one.c
@@ -6,10 +6,8 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 17:18:02 by moabdels          #+#    #+#             */
-/*   Updated: 2025/03/25 17:51:22 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/03/26 12:28:36 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_memsafety.h"
-
-// ? the size check is necessary because malloc(0) means a specific thing
