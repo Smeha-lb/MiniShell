caec74c0f6ea71319e163e7bd5ffa96b7dd9fd8e
moabdels <mkabdelsater@gmail.com>
2025-04-16 17:06:46 +0300

    Implement extract_lexemes in lexing.c

--- inc/lexing.h
+++ inc/lexing.h
@@ -6,16 +6,25 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 16:04:00 by moabdels          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/04/24 16:18:54 by moabdels         ###   ########.fr       */
+=======
+/*   Updated: 2025/04/16 16:04:42 by moabdels         ###   ########.fr       */
+>>>>>>> 43415cb (Implement extract_lexemes in lexing.c)
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXING_H
 # define LEXING_H
+<<<<<<< HEAD
 
 # include "../inc/minishell.h"
 
 t_token	parse_token(char chr, char next);
 int 	get_token_precedence(t_token token);
+=======
+
+# include "../inc/minishell.h"
+>>>>>>> 43415cb (Implement extract_lexemes in lexing.c)
 
 #endif
