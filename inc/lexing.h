3ebc8210699e5aa68d9188e6c261078034361c75
moabdels <mkabdelsater@gmail.com>
2025-04-21 16:48:15 +0300

    Add s_astree and s_redirect structs

--- inc/lexing.h
+++ inc/lexing.h
@@ -6,11 +6,7 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 16:04:00 by moabdels          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/04/24 16:18:54 by moabdels         ###   ########.fr       */
-=======
-/*   Updated: 2025/04/16 16:04:42 by moabdels         ###   ########.fr       */
->>>>>>> 43415cb (Implement extract_lexemes in lexing.c)
+/*   Updated: 2025/04/25 14:29:10 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +17,13 @@
 # include "../inc/minishell.h"
 
 t_token	parse_token(char chr, char next);
+<<<<<<< HEAD
 int 	get_token_precedence(t_token token);
 =======
 
 # include "../inc/minishell.h"
 >>>>>>> 43415cb (Implement extract_lexemes in lexing.c)
+=======
+>>>>>>> a8c7c55 (Add s_astree and s_redirect structs)
 
 #endif
