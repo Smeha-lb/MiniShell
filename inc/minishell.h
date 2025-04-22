003f9d12577c14932d06e6cc7b51065a398a3d4a
moabdels <mkabdelsater@gmail.com>
2025-04-22 15:17:25 +0300

    Add token_is_redir function to check if a token is a redirection operation

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:02:04 by moabdels          #+#    #+#             */
-/*   Updated: 2025/05/05 16:41:03 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/05/05 16:41:25 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,8 @@ typedef struct s_signal_handler
 extern	t_signal_handler	signal_handler;
 
 t_env_list	*create_env_list(char **env);
+
+bool	token_is_redir(t_token token);
 
 int		check_list_dup(char *data, t_env_list *head);
 
