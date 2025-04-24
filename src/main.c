8caba57b16018509b336cf2f0ec2de89df7273e8
moabdels <mkabdelsater@gmail.com>
2025-04-24 13:02:39 +0300

    Implement no_token_syntax_errs in lexing.c

--- src/main.c
+++ src/main.c
@@ -6,57 +6,13 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:02:12 by moabdels          #+#    #+#             */
-/*   Updated: 2025/04/25 14:27:30 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/04/25 14:31:03 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
 t_signal_handler	signal_handler;
-// line is a pointer to a string, not a 2d array
-// we love the C type system don't we folks?
-
-// ? we're saying here that we don't care about interrupts,
-// ?  but we do care about other errors.
-// * `return (free(temp), false)` -> this works because:
-// * 1 - return takes any expression
-// * 2 - comma is an operator that takes any number of expressions, evaluates
-// * them, then discards everything but the rightmost expression.
-
-static bool	get_input(char **line, bool update_hist, bool is_tty)
-{
-	char	*temp;
-
-	errno = OK;
-	if (is_tty)
-		*line = readline("minishell%%> ");
-	else
-	{
-		temp = get_next_line(STDIN_FILENO);
-		if (errno != OK)
-			return (free(temp), false);
-		if (temp)
-		{
-			*line = ft_strtrim(temp, "\n");
-			free(temp);
-		}		
-	}
-	if (errno == EINTR)
-		errno = OK;
-	else if (errno != OK)
-		return (false);
-	if (update_hist && *line && **line)
-		add_history(*line);
-	return (errno == OK);
-}
-
-// TODO: create a function to turn env to a linked list
-// ! should main take envp?
-int	main(void)
-{
-	char	*input;
-	int		fd_in;
-	int		fd_out;
 
 static pid_t	get_pid(void)
 {
@@ -132,11 +88,6 @@ int	main(void)
 	bool		valid_input;
 	t_minishell	minishell;
 	
-	shell_init(&minishell);
-	bool		valid_input;
-	t_minishell	minishell;
-	
-	minishell = NULL;
 	shell_init(&minishell);
 	while (true)
 	{
