e65039693233f802d6bd05999f0d06488e4bf834
moabdels <mkabdelsater@gmail.com>
2025-04-28 15:26:27 +0300

    Implement get_cmd and all it's attendant helper functions

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:02:04 by moabdels          #+#    #+#             */
-/*   Updated: 2025/05/05 17:01:43 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/05/05 17:02:10 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@
 # define EX_BAD_USAGE 258
 # define WHITESPACE " \t\n\v\r\f"
 # define UNEX_TOKEN "Syntax Error near unexpected token"
+# define MSH_DEBUG "\e[1;93minfo:\e[0m "
+# define MSH_ERR "\e[1;31mminishell:\e[0m "
 
 typedef struct s_env_list{
 	char *name;
