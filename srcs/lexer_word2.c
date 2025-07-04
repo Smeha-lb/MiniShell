a0d78afbe39a9ac865c98f83400b42e1facce187
Carl Samaha <carlsamaha1@gmail.com>
2025-07-04 13:17:57 +0300

    fixed issue with single quotes still expanding

--- srcs/lexer_word2.c
+++ srcs/lexer_word2.c
@@ -44,12 +44,14 @@ int	handle_word(char *input, int *i, t_shell *shell)
 	return (handle_complex_word(input, i, shell, 0));
 }
 
-int	process_word_loop(char *input, int *i, char *word, t_shell *shell)
+int	process_word_loop(char *input, int *i, char *word, t_shell *shell, int *quote_type)
 {
 	int				j;
+	int				result;
 	t_temp_var_data	data;
 
 	j = 0;
+	*quote_type = 0;
 	while (input[*i] && !is_word_delimiter(input[*i], input[*i + 1]))
 	{
 		if (input[*i] == '$' && input[*i + 1] == '\"')
@@ -58,8 +60,11 @@ int	process_word_loop(char *input, int *i, char *word, t_shell *shell)
 		data.i = i;
 		data.j = &j;
 		data.word = &word;
-		if (process_word_character(data, shell))
+		result = process_word_character(data, shell);
+		if (result < 0)
 			return (-1);
+		if (result > 0 && *quote_type == 0)
+			*quote_type = result;
 	}
 	word[j] = '\0';
 	return (j);
@@ -69,6 +74,7 @@ int	handle_complex_word(char *input, int *i, t_shell *shell, int quoted)
 {
 	char			*word;
 	int				buffer_size;
+	int				quote_type;
 	t_word_data		word_data;
 
 	buffer_size = calculate_word_buffer_size(input, *i);
@@ -76,12 +82,14 @@ int	handle_complex_word(char *input, int *i, t_shell *shell, int quoted)
 	if (!word)
 		return (1);
 	prepare_word_data(input, i, &word_data);
-	word_data.j = process_word_loop(input, i, word, shell);
+	word_data.j = process_word_loop(input, i, word, shell, &quote_type);
 	if (word_data.j < 0 || word_data.j >= buffer_size)
 	{
 		free(word);
 		return (1);
 	}
+	if (quote_type > 0)
+		quoted = quote_type;
 	add_to_token_list(&shell->tokens, create_token(word, TOKEN_WORD, quoted));
 	free(word);
 	return (0);
